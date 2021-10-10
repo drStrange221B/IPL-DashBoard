eec27dfb5643ecc5322ce39bc274d04921d0fb5f
Rojaya Maharjan <rojaya.mh3@gmail.com>
2021-10-10 19:45:56 -0400

    adding match API

--- ipl-dashboard/src/main/java/com/emrys/ipldashboard/controller/TeamController.java
+++ ipl-dashboard/src/main/java/com/emrys/ipldashboard/controller/TeamController.java
@@ -1,12 +1,14 @@
 package com.emrys.ipldashboard.controller;
 
+import com.emrys.ipldashboard.model.Match;
 import com.emrys.ipldashboard.model.Team;
 import com.emrys.ipldashboard.repository.MatchRepository;
 import com.emrys.ipldashboard.repository.TeamRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -27,4 +29,17 @@ public class TeamController {
 
         return team;
     }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year){
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+
+//        return this.matchRepository.getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(teamName,startDate, endDate, teamName, startDate, endDate);
+
+        return this.matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
+
+    }
+
+
 }
