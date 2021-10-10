f7795e792ca1a1a889134506945d9d8e2c252eb4
Rojaya Maharjan <rojaya.mh3@gmail.com>
2021-10-10 12:58:35 -0400

    adding router and links in React

--- ipl-dashboard/src/frontend/src/components/MatchSmallCard.js
+++ ipl-dashboard/src/frontend/src/components/MatchSmallCard.js
@@ -1,9 +1,16 @@
-import {React} from 'react';
+import { React } from 'react';
+import {Link} from 'react-router-dom'
 
-export const MatchSmallCard = ({ match }) => {
+export const MatchSmallCard = ({ teamName, match }) => {
+  if (!match) return null;
+  
+  const otherTeam = teamName === match.team1 ? match.team2 : match.team1;
+  const otherTeamRoute = `/teams/${otherTeam}`;
+
   return (
     <div className="MatchSmallCard">
-      <p>{match.team1} VS {match.team2} </p>
+      <h3>VS <Link to={otherTeamRoute}>{otherTeam} </Link></h3>
+      <p>{match.matchWinner} won by {match.resultMargin} {match.result}</p>
     </div>
   );
 }
