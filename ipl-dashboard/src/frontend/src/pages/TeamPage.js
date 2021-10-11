cf67f73652a4c70256909c37e23b79342422a1a4
Rojaya Maharjan <rojaya.mh3@gmail.com>
2021-10-10 21:33:45 -0400

    adding match API amd routing for match page

--- ipl-dashboard/src/frontend/src/pages/TeamPage.js
+++ ipl-dashboard/src/frontend/src/pages/TeamPage.js
@@ -1,37 +1,32 @@
-import {React, useEffect, useState } from 'react';
-import {MatchDetailCard} from '../components/MatchDetailCard';
-import { MatchSmallCard } from '../components/MatchSmallCard';
-import { useParams } from 'react-router-dom';
+import { React, useEffect, useState } from "react";
+import { MatchDetailCard } from "../components/MatchDetailCard";
+import { MatchSmallCard } from "../components/MatchSmallCard";
+import { useParams } from "react-router-dom";
 
 export const TeamPage = () => {
-
   const { teamName } = useParams();
   const [team, setTeam] = useState({ matches: [] });
-  
 
-  useEffect(
-
-    () => {
-      const fetchMatches = async () => {
-        const response = await fetch(`http://localhost:8080/team/${teamName}`);
-        const data = await response.json();
-        setTeam(data);
-      };
-      fetchMatches();
-    },[teamName]
-  );
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const response = await fetch(`http://localhost:8080/team/${teamName}`);
+      const data = await response.json();
+      setTeam(data);
+    };
+    fetchMatches();
+  }, [teamName]);
 
   if (!team || !team.teamName) {
-    return <h1>Team not found</h1>
+    return <h1>Team not found</h1>;
   }
-  
+
   return (
     <div className="TeamPage">
       <h1>{team.teamName}</h1>
-      <MatchDetailCard teamName={team.teamName} match = {team.matches[0]}/>
-      {team.matches.slice(1).map(match => <MatchSmallCard key={match.id} teamName={team.teamName} match ={match}/>)}
+      <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      {team.matches.slice(1).map((match) => (
+        <MatchSmallCard key={match.id} teamName={team.teamName} match={match} />
+      ))}
     </div>
   );
-}
-
-
+};
