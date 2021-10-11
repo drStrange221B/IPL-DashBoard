9a3741d024aa59856db90aed2058d126144df554
Rojaya Maharjan <rojaya.mh3@gmail.com>
2021-10-11 02:34:36 -0400

    adding style on matches page

--- ipl-dashboard/src/frontend/src/components/MatchSmallCard.js
+++ ipl-dashboard/src/frontend/src/components/MatchSmallCard.js
@@ -1,18 +1,27 @@
-import { React } from 'react';
-import {Link} from 'react-router-dom'
+import { React } from "react";
+import { Link } from "react-router-dom";
+import "./MatchSmallCard.scss";
 
 export const MatchSmallCard = ({ teamName, match }) => {
   if (!match) return null;
-  
+
   const otherTeam = teamName === match.team1 ? match.team2 : match.team1;
   const otherTeamRoute = `/teams/${otherTeam}`;
+  const isMatchWon = teamName === match.matchWinner;
 
   return (
-    <div className="MatchSmallCard">
-      <h3>VS <Link to={otherTeamRoute}>{otherTeam} </Link></h3>
-      <p>{match.matchWinner} won by {match.resultMargin} {match.result}</p>
+    <div
+      className={
+        isMatchWon ? "MatchSmallCard won-card" : "MatchSmallCard lost-card"
+      }
+    >
+      <span className="vs">VS </span>
+      <h1>
+        <Link to={otherTeamRoute}>{otherTeam} </Link>
+      </h1>
+      <p className="match-result">
+        {match.matchWinner} won by {match.resultMargin} {match.result}
+      </p>
     </div>
   );
-}
-
-
+};
